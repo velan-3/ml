900d44749134c692e746a806056f0c34bff27e34
velan-3 <106436639+velan-3@users.noreply.github.com>
2023-07-26 14:26:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 25 20:52:23 2023
-
-@author: Velan
-"""
-
 import numpy as np
 from flask import Flask,jsonify,render_template
 import pickle
@@ -16,17 +9,6 @@ model = pickle.load(open('model.pkl', 'rb'))
 def index():
     return render_template('index.html')
 
-#
-#@app.route('/predict_api',methods=['POST'])
-#def predict_api():
-   # '''
-    #For direct API calls trought request
-    #'''
-    #data = request.get_json(force=True)
-    #prediction = model.predict([np.array(list(data.values()))])
-
-    #output = prediction[0]
-    #return jsonify(output)
 @app.route('/predict_api/<int:feature1>/<int:feature2>/<int:feature3>/<int:feature4>', methods=['GET'])
 def predict_api(feature1, feature2, feature3, feature4):
     # Convert the input features to a numpy array
