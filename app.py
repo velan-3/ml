682324f4c083cc1579640e436e55909cd0ad81b7
velan-3 <106436639+velan-3@users.noreply.github.com>
2023-07-29 16:46:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,10 +37,10 @@ def predict():
     
     return render_template('index.html',prediction_text='Premium amount is{}'.format(output))
 
-@app.route('/predict_api/<int:feature1>/<int:feature2>/<int:feature3>/<int:feature4>', methods=['GET'])
-def predict_api(feature1, feature2, feature3, feature4):
+@app.route('/predict_api/<int:feature1>/<int:feature2>/<int:feature3>/<int:feature4>/<int:feature5>', methods=['GET'])
+def predict_api(feature1, feature2, feature3, feature4, feature5):
     # Convert the input features to a numpy array
-    input_data = np.array([[feature1, feature2, feature3, feature4]])
+    input_data = np.array([[feature1, feature2, feature3, feature4, feature5]])
 
     # Make the prediction using the model
     prediction = model.predict(input_data)
